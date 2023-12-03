725a378faa8b576a5dbbd1e5fd5c91026f0a088d
Rahul phatangare <rahulphatangare01@gmail.com>
2023-12-04 00:12:16 +0530

    home section created

--- src/components/Layout/home/Home.js
+++ src/components/Layout/home/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
-import './Home.css'
+import './Home.css';
+import Product from "../product/Product";
 const Home =()=>{
 
+    const product = {
+        name:'Blue Tshirt',
+        images:[{url:'https://i.ibb.co/DRST11n/1.webp'}],
+        price:'$300',
+        _id:"rahul"
+    }
     return(
         <>
             <div className="banner">
@@ -11,6 +18,12 @@ const Home =()=>{
             <button> Scroll</button>
 
             </a>
+
+            </div>
+
+            <h2 className="homeHeading">Featured Products</h2>
+            <div className="container" id="container">
+            <Product  Product= {product}  />
 
             </div>
         </>
